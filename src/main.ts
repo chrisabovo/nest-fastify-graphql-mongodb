4359079a10dd1f4e6598fbc6b32d8d45c9c03d5d
christiano.bovo <christiano.bovo@sonda.com>
2019-08-14 20:24:00 +0000

    versão inicial

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const port = 3333;
+  const url = `http://localhost:${port}`;
+
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  await app.listen(port);
+
+  console.log(`Server ready at ${url}`);
+}
+bootstrap();
