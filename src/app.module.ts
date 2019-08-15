3666cf3c5e6ebe372cc17072681b5bad180fcb72
chrisabovo <chrisabovo@gmail.com>
2019-08-14 23:55:22 -0300

    inclusão do graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from "@nestjs/common";
+import {GraphQLModule} from "@nestjs/graphql";
+import {join} from "path";
+import {AppController} from "./app.controller";
+import {AppService} from "./app.service";
 
 @Module({
-  imports: [],
+  imports: [GraphQLModule.forRoot({
+      debug: true,
+      playground: true,
+      typePaths: ["./**/*.graphql"],
+      definitions: {
+        path: join(process.cwd(), "src/graphql.ts"),
+        outputAs: "class"
+      }
+    })],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,12 @@
-import { NestFactory } from '@nestjs/core';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
-import { AppModule } from './app.module';
+import {NestFactory} from "@nestjs/core";
+import {FastifyAdapter, NestFastifyApplication} from "@nestjs/platform-fastify";
+import {AppModule} from "./app.module";
 
 async function bootstrap() {
   const port = 3333;
   const url = `http://localhost:${port}`;
 
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter({ logger: true }),
-  );
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter({logger: true}));
   await app.listen(port);
 
   console.log(`Server ready at ${url}`);
